48d976d7adf93495efbd110bf5d80e651d75dd32
Mireia Maldonado Parra <mirmalparra@gmail.com>
2023-02-01 17:40:34 +0100

    Add event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'my-form';
   name!:string;
   cities = ["Sevilla", "Córdoba", "Huelva"];
+  selection!:string;
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log('Change', changes)
+  // }
+
+  ngOnInit() : void {
+    console.log('OnInit')
+  }
+
+   ngOnDestroy() : void {
+    console.log('OnInit')
+  }
+
+  onCityClicked(city:string):void {
+    console.log('City', city);
+    this.selection = city;
+  }
+
+   onClear():void {
+    this.selection = "";
+  }
 }

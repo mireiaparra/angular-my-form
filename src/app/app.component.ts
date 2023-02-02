d74d5a3362d0682f60fc2f9bd43dcb4466b3ee2a
Mireia Maldonado Parra <mirmalparra@gmail.com>
2023-02-02 10:57:13 +0100

    Add input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent {
   title = 'my-form';
   name!:string;
   cities = ["Sevilla", "Córdoba", "Huelva"];
   selection!:string;
 
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   console.log('Change', changes)
-  // }
 
-  ngOnInit() : void {
-    console.log('OnInit')
-  }
-
-   ngOnDestroy() : void {
-    console.log('OnInit')
-  }
 
   onCityClicked(city:string):void {
     console.log('City', city);
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  templateUrl: './button.component.html',
+  template: `<button [ngStyle]="{'background-color': color}">{{label}}</button>`,
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit{
+export class ButtonComponent implements OnChanges, OnInit, OnDestroy{
+  @Input() color!:string;
+  @Input() label!:string;
 constructor() {}
-ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit');
+    
+  }
   
-}
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
+
 }
